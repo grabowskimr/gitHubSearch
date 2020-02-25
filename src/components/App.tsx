6a630f5a765360419bdd5452629ef6bf7d3030b3
grabeqx <grabowski.kontakt@gmail.com>
2020-02-25 13:53:08 +0100

    App title

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,11 +4,13 @@ import Search from './Search';
 import ErrorMessage from './ErrorMessage';
 import ContextProvider from './ContextProvider';
 import UserList from '../containers/UserList';
+import AppTitle from '../containers/AppTitle';
 
 const App: React.FC = (): JSX.Element => {
     return (
         <div className="App">
             <ContextProvider>
+                <AppTitle />
                 <Search />
                 <UserList />
                 <ErrorMessage />
---
+++ src/containers/AppTitle.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AppTitle = (): JSX.Element => (
+    <h1 className="app-title">
+        <span className="bold-text">Github</span>Search
+    </h1>
+);
+
+export default AppTitle;
